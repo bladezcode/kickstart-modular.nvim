b5e754d0049e96f32ec9ab87a1cea24d30cfb929
bladezcode <meugene992@gmail.com>
2024-10-28 22:25:49 +0800

    experimented with dashboard.nvim for diff theme

--- lua/custom/plugins/welcome.lua
+++ lua/custom/plugins/welcome.lua
@@ -27,7 +27,20 @@ return {
     -- custom config
     vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#b4d4ad' })
     require('dashboard').setup {
+      -- theme = 'doom',
       config = {
+        center = {
+          {
+            icon = '',
+            icon_hl = 'group',
+            desc = 'description',
+            desc_hl = 'group',
+            key = 'shortcut key in dashboard buffer not keymap !!',
+            key_hl = 'group',
+            key_format = ' [%s]', -- `%s` will be substituted with value of `key`
+            action = '',
+          },
+        },
         header = vim.split(string.rep('\n', 3) .. logo, '\n'),
         shortcut = {
           { desc = '[ Github]', group = 'DashboardShortCut' },
